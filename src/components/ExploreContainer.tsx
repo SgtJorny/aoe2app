e9c41e8831379505cb51eeafc48b2f5478250a79
Levi Joraanstad <ljoraanstad@gmail.com>
2020-12-02 10:46:14 -0600

    Updating tabs

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import './ExploreContainer.css';
-import { IonList, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent } from '@ionic/react';
+import React from "react";
+import "./ExploreContainer.css";
+import {
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonToggle,
+  IonRadio,
+  IonCheckbox,
+  IonItemSliding,
+  IonItemOption,
+  IonItemOptions,
+  IonContent
+} from "@ionic/react";
 
 interface ContainerProps {
   name: string;
@@ -9,68 +21,67 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <IonContent>
-    {/*-- List of Text Items --*/}
-    <IonList>
-      <IonItem>
-        <IonLabel>Pokémon Yellow</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Mega Man X</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>The Legend of Zelda</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Pac-Man</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Super Mario World</IonLabel>
-      </IonItem>
-    </IonList>
-
-    {/*-- List of Input Items --*/}
-    <IonList>
-      <IonItem>
-        <IonLabel>Input</IonLabel>
-        <IonInput></IonInput>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Toggle</IonLabel>
-        <IonToggle slot="end"></IonToggle>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Radio</IonLabel>
-        <IonRadio slot="end"></IonRadio>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Checkbox</IonLabel>
-        <IonCheckbox slot="start" />
-      </IonItem>
-    </IonList>
-
-    {/*-- List of Sliding Items --*/}
-    <IonList>
-      <IonItemSliding>
+      {/*-- List of Text Items --*/}
+      <IonList>
         <IonItem>
-          <IonLabel>Item</IonLabel>
+          <IonLabel>{name}Pokémon Yellow</IonLabel>
         </IonItem>
-        <IonItemOptions side="end">
-          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
-        </IonItemOptions>
-      </IonItemSliding>
-
-      <IonItemSliding>
         <IonItem>
-          <IonLabel>Item</IonLabel>
+          <IonLabel>Mega Man X</IonLabel>
         </IonItem>
-        <IonItemOptions side="end">
-          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
-        </IonItemOptions>
-      </IonItemSliding>
-    </IonList>
-  </IonContent>
+        <IonItem>
+          <IonLabel>The Legend of Zelda</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Pac-Man</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Super Mario World</IonLabel>
+        </IonItem>
+      </IonList>
+
+      {/*-- List of Input Items --*/}
+      <IonList>
+        <IonItem>
+          <IonLabel>Input</IonLabel>
+          <IonInput></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Toggle</IonLabel>
+          <IonToggle slot="end"></IonToggle>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Radio</IonLabel>
+          <IonRadio slot="end"></IonRadio>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Checkbox</IonLabel>
+          <IonCheckbox slot="start" />
+        </IonItem>
+      </IonList>
+
+      {/*-- List of Sliding Items --*/}
+      <IonList>
+        <IonItemSliding>
+          <IonItem>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+          <IonItemOptions side="end">
+            <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+          </IonItemOptions>
+        </IonItemSliding>
+
+        <IonItemSliding>
+          <IonItem>
+            <IonLabel>Item</IonLabel>
+          </IonItem>
+          <IonItemOptions side="end">
+            <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+          </IonItemOptions>
+        </IonItemSliding>
+      </IonList>
+    </IonContent>
   );
-  
 };
 
 export default ExploreContainer;
@@ -82,7 +93,7 @@ export default ExploreContainer;
 //       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
 //     </div>
 //   );
-  
+
 // };
 
 // export default ExploreContainer;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab2.css";
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Builds</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Builds</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
